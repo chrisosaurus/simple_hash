4ecbb2bae98242c12c4f80010d06763f22845e8f
Chris Hall <followingthepath@gmail.com>
2016-12-19 20:45:51 +1100

    Adding explicit tests for both branches of `sh_set`

--- test_simple_hash.c
+++ test_simple_hash.c
@@ -183,6 +183,107 @@ void update(void){
     puts("success!");
 }
 
+void set(void){
+    /* set works for both new and existing keys */
+    /* our simple hash table */
+    struct sh_table *table = 0;
+
+    /* some keys */
+    char *key_1 = "rhubarb";
+    char *key_2 = "carrot";
+    char *key_3 = "potato";
+
+    /* some data */
+    int data_1 = 1;
+    int data_2 = 2;
+    int data_3 = 3;
+
+    /* some data we override with */
+    int new_data_1 = 14;
+    int new_data_2 = 15;
+    int new_data_3 = 16;
+
+
+    /* temporary data pointer used for testing get */
+    int *data = 0;
+
+    puts("\ntesting update functionality");
+
+    puts("creating table");
+    table = sh_new(32);
+    assert(table);
+    assert( 32 == table->size );
+    assert( 0 == sh_nelems(table) );
+
+
+    puts("inserting some data using set");
+    assert( sh_set(table, key_1, &data_1) );
+    assert( 1 == sh_nelems(table) );
+    assert( 0 == sh_get(table, key_2) );
+    assert( 0 == sh_get(table, key_3) );
+
+    data = sh_get(table, key_1);
+    assert(data);
+    assert( data_1 == *data );
+
+
+    assert( sh_set(table, key_2, &data_2) );
+    assert( 2 == sh_nelems(table) );
+    assert( 0 == sh_get(table, key_3) );
+
+    data = sh_get(table, key_2);
+    assert(data);
+    assert( data_2 == *data );
+
+
+    assert( sh_set(table, key_3, &data_3) );
+    assert( 3 == sh_nelems(table) );
+
+    data = sh_get(table, key_3);
+    assert(data);
+    assert( data_3 == *data );
+
+
+    puts("testing update via set");
+
+    puts("two update");
+    assert(sh_set(table, key_2, &new_data_2));
+    data = sh_get(table, key_2);
+    assert(data);
+    assert( *data == new_data_2 );
+    assert( 3 == sh_nelems(table) );
+
+    data = sh_get(table, key_2);
+    assert(data);
+    assert( *data == new_data_2 );
+
+    puts("three update");
+    assert(sh_set(table, key_3, &new_data_3));
+    data = sh_get(table, key_3);
+    assert(data);
+    assert( *data == new_data_3 );
+    assert( 3 == sh_nelems(table) );
+
+    data = sh_get(table, key_3);
+    assert(data);
+    assert( *data == new_data_3 );
+
+    puts("one update");
+    assert(sh_set(table, key_1, &new_data_1));
+    data = sh_get(table, key_1);
+    assert(data);
+    assert( *data == new_data_1 );
+    assert( 3 == sh_nelems(table) );
+
+    data = sh_get(table, key_1);
+    assert(data);
+    assert( *data == new_data_1 );
+
+
+    assert( sh_destroy(table, 1, 0) );
+    puts("success!");
+}
+
 void delete(void){
     /* our simple hash table */
     struct sh_table *table = 0;
@@ -770,6 +871,8 @@ int main(void){
     new_insert_get_destroy();
 
     update();
+
+    set();
 
     delete();
 
